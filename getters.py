d5454170157303084b8a23a3ac130de669d0a1ae
MacBook Air <mehdibenchaji@gmail.com>
2022-08-19 09:13:19 +0100

    version mehdi

--- getters.py
+++ getters.py
@@ -1,85 +1,111 @@
 import random
 import datetime
 import time
-import decimal
 import os
+import re
 from configparser import ConfigParser
+from dict import DICTIONNAIRE
 
-# le chemin absolu du fichier
+#TYPE, FORMAT, VALUES = 0,1,2
+
+# le chemin absolut du fichier
 absPath = os.path.dirname(os.path.realpath(__file__))
 configFile = os.path.join(absPath, 'config.conf')
 data = ConfigParser()
 # charger le fichier de conf
 data.read(configFile)
 
-# fonction pour obtenir les infos sur le fichier
+# les sections
+sectionName = list(data.sections())
+
+# les sous sections de la première section
+fieldnames = list(data[sectionName[0]].keys())
+
+#verifier les patterns du conf values à partir du dict
+def regex_checker(val, dtype):
+    if dtype in DICTIONNAIRE:
+        regex_pattern = DICTIONNAIRE[dtype]
+        pattern = re.compile(regex_pattern)
+        if not pattern.match(str(val)):
+            raise ValueError(f"{val} doesn't match the correspondent pattern of {dtype} in dict.py")
+    else:
+        raise ValueError(f"{dtype} type isn't included in dict.py")
+
+# fonction pour obtenir les infos sur le fichier CSV
 def getFileInfo(undersection):
     field = data['fileInfos'][undersection]
     return field
 
-# Fonctions pour la conversion des types
+# convertir au type souhaité
+def convertTo(dval, dtype, limit=None):
+    res = []
+    res = [dtype(x) for x in dval.split('-')]
+    if not limit == None:
+        if len(res) == limit:
+            return res
+        else:
+            raise TypeError(f"'{dval}' is not the Length of {limit}")
+            return None
+    return res
 
-def date(year):
-    sdate = datetime.date(int(year),1,1)
+# obtenir un entier aleatoire
+def getRandomInt(val,form=""):
+    start , end = convertTo(val, int, 2)
+    return random.randint(start, end)
+
+# float aleatoire
+def getRandomFloat(val, form=""):
+    start , end = convertTo(val, float, 2)
+    randomFloat = lambda x,y : round(random.uniform(x, y),2)
+    return randomFloat(start, end)
+
+# string aleatoire
+def getRandomString(val,form=""):
+    strings = convertTo(val, str)
+    return random.choice(strings)
+
+def date(d):
+    day, month, year = d.split("/")
+    sdate = datetime.date(int(year),int(month),int(day))
     # generer les timestamps
     return time.mktime(sdate.timetuple())
 
-# retourner une liste du type souhaite
-def convertTo(inData, dtype):
-    return [dtype(x) for x in inData.split()]
-
-# retourner une date aleatoire entre 2 annees (ex: entre 2002 et 2021)
-def getRandomDate(dateTime,form=""):
-    start , end = convertTo(dateTime,date) # date c'est une fonction
+# retourner une date aleatoire
+def getRandomDate(val,form=""):
+    start , end = convertTo(val, date, 2)
     randomDate =random.randint(start, end)
     return datetime.datetime.fromtimestamp(randomDate).strftime(form)
-
-# obtenir un entier aleatoire
-def getRandomInt(intSet,form=""):
-    start , end = convertTo(intSet, int)
-    return random.randint(start, end)
-
-# list aleatoire
-def getRandomSample(inSet, number=2,form=""):
-    return random.sample(inSet.split(), number)
-
-# string aleatoire
-def getRandomString(intSet,form=""):
-    strings = convertTo(intSet, str)
-    return random.choice(strings)
-
-# decimal aleatoire
-def getRandomDecimal(inDecimals, form=""):
-    start , end = convertTo(inDecimals, int)
-    randomDecimal = lambda x,y : float(decimal.Decimal(random.randrange(x,y))/100)
-    return randomDecimal(start, end)
 
 # timestamp aleatoire
-def getTimestamp(dateTime,form=""):
-    start , end = convertTo(dateTime, int)
-    randomDate =random.randint(start, end)
+def getTimestamp(val,form=""):
+    start, end = convertTo(val, date, 2)
+    randomDate = random.randint(int(start), int(end))
     return datetime.datetime.fromtimestamp(randomDate).strftime(form)
+
+# list aleatoire
+def getRandomSample(val, number=2,form=""):
+    return random.sample(val.split(), number)
 
 
 # les types possibles
 dataTypes = {'int': getRandomInt,
              'string': getRandomString,
+             'float': getRandomFloat,
              'timestamp': getTimestamp,
-             'decimal': getRandomDecimal,
              "datetime": getRandomDate,
-             'list': getRandomSample
+             'list': getRandomSample,
+             'boolean': getRandomString, # Vu que ca contient yes/no
              }
 
 # fonction pour obtenir/generer les ranges
 def getData(fieldname):
-    field = data[fieldname]
-    dtype = field['type']
-    if dtype in dataTypes:
-        return dataTypes[dtype](field['values'],form=field['format'])
-    return field['values']
-
-sectionName = list(data.sections())
-fieldnames = list(data[sectionName[0]].keys())
-
-
-print(fieldnames)
+    field = data[sectionName[0]][fieldname].split(',')
+    if len(field) == 3:
+        dtype = field[0]
+        if dtype in dataTypes:
+            regex_checker(field[2], dtype)
+            return dataTypes[dtype](field[2],form=field[1])
+        else:
+            raise TypeError(f'{dtype} is not supported')
+    else:
+        raise IndexError(f'{fieldname} list index out of range')
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import csv
 import time
-from getters import *  # Importing all of functions from our library getters.py
+from getters import * # Importing all of functions from our library getters.py
+import os
+import traceback
 
-start = time.time()
-# Nom du fichier horodate
+# Nom du fichier horodaté
 dateFile = time.strftime(getFileInfo("dateFormat"))
 fileType = getFileInfo("fileType")
 filename = "%s_%s.%s" % (getFileInfo("genre"), dateFile, fileType)
 
-# les colonnes
-fieldnames = data['schema']['fieldnames'].split()
-
+# le nombre d'enregistrement à partir de la section FileInfo
 maxRecords = int(getFileInfo("maxRecords"))
 
-with open(filename, 'w+', newline='') as f:
+with open(filename, 'w', newline='') as f:
     file = csv.writer(f)
     file.writerow(fieldnames)
-
-    for i in range(1, maxRecords):
-        row = []
-        for field in fieldnames:
-            row.append(getData(field))
-        file.writerow(row)
-
-end = time.time()
-print(end - start)
+    current_id = 1
+    try:
+        for i in range(1, maxRecords):
+            row = [current_id]
+            for field in fieldnames:
+                row.append(getData(field))
+            file.writerow(row)
+            current_id += 1
+        print("csv created successfuly")
+    except:
+        os.remove(filename) # Remove file if any error occurs
+        traceback.print_exc()
