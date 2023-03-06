f68306b022a373abe29b4cbc5e397b80974117d2
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-06 07:35:32 -0800

    xlsx

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-import csv
 import time
-from getters import * # Importing all of functions from our library getters.py
 import os
 import traceback
+import openpyxl
+from openpyxl.utils import get_column_letter
+from getters import *
 
 start = time.time()
+
 # Nom du fichier horodaté
 dateFile = time.strftime(getFileInfo("dateFormat"))
 fileType = getFileInfo("fileType")
-path=getFileInfo("path")
-filename = "%s\%s_%s.%s" % (path,getFileInfo("genre"), dateFile, fileType)
+path = getFileInfo("path")
+filename = "%s\%s_%s.%s" % (path, getFileInfo("genre"), dateFile, fileType)
 
 # les colonnes
-section=getsection()
-fieldnames=getkeys(section[0])
-
+section = getsection()
+fieldnames = getkeys(section[0])
 
 maxRecords = int(getFileInfo("maxRecords"))
 
-with open(filename, 'w+', newline='') as f:
-    file = csv.writer(f)
-    file.writerow(fieldnames)
+# création d'un classeur
+wb = openpyxl.Workbook()
 
-    for j in range(0, maxRecords):
-        row = []
-        for i in range(0,len(fieldnames)):
-            row.append(getData(fieldnames[i]))
-        file.writerow(row)
+# création d'une feuille
+ws = wb.active
+
+# écrire les en-têtes de colonne
+for i, fieldname in enumerate(fieldnames):
+    col_letter = get_column_letter(i + 1)
+    ws.cell(row=1, column=i+1, value=fieldname)
+
+# écrire les données
+for j in range(1, maxRecords+1):
+    row = []
+    for fieldname in fieldnames:
+        row.append(getData(fieldname))
+    ws.append(row)
+
+# enregistrer le classeur
+wb.save(filename)
 
 end = time.time()
-print("CSV généré en "+ str(end - start) +" secondes")
+print("XLSX généré en " + str(end - start) + " secondes")
