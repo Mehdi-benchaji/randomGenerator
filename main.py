1d801e69461330b379ad6fcbd9b49fa9c992dea8
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-06 07:22:13 -0800

    do

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import csv
 import time
-from getters import * # Importing all of functions from our library getters.py
 import os
 import traceback
+from openpyxl import Workbook
+from getters import * # Importing all of functions from our library getters.py
 
 start = time.time()
+
 # Nom du fichier horodaté
 dateFile = time.strftime(getFileInfo("dateFormat"))
 fileType = getFileInfo("fileType")
@@ -13,18 +14,22 @@ filename = "%s_%s.%s" % (getFileInfo("genre"), dateFile, fileType)
 # le nombre d'enregistrement à partir de la section FileInfo
 maxRecords = int(getFileInfo("maxRecords"))
 
-with open(filename, 'w', newline ='') as f:
-    file = csv.writer(f)
-    file.writerow(fieldnames)
-    try:
-        for i in range (0,maxRecords+1) :
-            row = []
-            for field in fieldnames:
-                row.append(getData(field))
-            file.writerow(row)
-    except:
-        os.remove(filename) # Remove file if any error occurs
-        traceback.print_exc()
+wb = Workbook()
+ws = wb.active
+
+try:
+    ws.append(fieldnames)
+    for i in range(0, maxRecords):
+        row = []
+        for field in fieldnames:
+            row.append(getData(field))
+        ws.append(row)
+
+    wb.save(filename)
+
+except:
+    os.remove(filename) # Remove file if any error occurs
+    traceback.print_exc()
 
 end = time.time()
-print("CSV généré en "+ str(end - start) +" secondes")
+print("XLSX généré en "+ str(end - start) +" secondes")
